3c3f5aa0e0476baadd3c77b1f84856cced13308e
Xiaokun Zhu <betahust@gmail.com>
2015-08-27 13:28:49 +0800

    add admin_auth_url in local_settings

--- examples/local_settings.sample.py
+++ examples/local_settings.sample.py
@@ -4,5 +4,6 @@
 username = 'admin'
 password = 'admin'
 tenant_name = 'admin'
+admin_auth_url = 'http://controller:35357'
 auth_url = 'http://controller:5000' # discover api version
 auth_url_v20 = 'http://controller:5000/v2.0'
