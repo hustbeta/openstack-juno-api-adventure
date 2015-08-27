eb2ac2e982f731bf57734582c4ccecae158f8be3
Xiaokun Zhu <betahust@gmail.com>
2015-08-27 18:13:35 +0800

    auth using session

--- examples/keystone/v3/01_auth.py
+++ examples/keystone/v3/01_auth.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Authenticate using username/password, or using token"""
+"""Authenticate using username/password, or using token, with or without session.
+
+Note that auth without session is deprecated.
+"""
 import keystoneclient
+import keystoneclient.auth.identity.v3
+import keystoneclient.session
 import keystoneclient.v3.client
 
 import local_settings
@@ -17,6 +22,22 @@ def auth_user(username, password, project_name):
         return None
     return keystone
 
+def auth_user_with_session(username, password, project_name):
+    """Authenticate using username/password
+
+    This method won't fail upon bad username/password, annoying.
+    """
+    auth = keystoneclient.auth.identity.v3.Password(auth_url=local_settings.auth_url_v3,
+                                                    username=username,
+                                                    password=password,
+                                                    user_domain_name='Default',
+                                                    project_domain_name='Default',
+                                                    project_name=project_name)
+    session = keystoneclient.session.Session(auth=auth)
+    keystone = keystoneclient.v3.client.Client(session=session)
+
+    return keystone
+
 def auth_token(token):
     """Authenticate using token"""
     try:
@@ -26,6 +47,19 @@ def auth_token(token):
         return None
     return keystone
 
-keystone = auth_token('06ec8f8797004f25a4d75273afc48568')
+def auth_token_with_session(token):
+    """Authenticate using token."""
+    # TODO Failed, need investigating.
+    auth = keystoneclient.auth.identity.v3.Token(auth_url=local_settings.auth_url_v3,
+                                                 token=token)
+    session = keystoneclient.session.Session(auth=auth)
+    keystone = keystoneclient.v3.client.Client(session=session)
+
+    return keystone
+
+keystone = auth_user_with_session(local_settings.username,
+                                  local_settings.password,
+                                  local_settings.tenant_name)
 print keystone
+print keystone.domains.list()
 
