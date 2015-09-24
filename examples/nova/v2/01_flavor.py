dc03940b7fb8f3d3fb5d345403cff03de5ac2828
Xiaokun Zhu <betahust@gmail.com>
2015-09-24 15:23:56 +0800

    nova flavor

---
+++ examples/nova/v2/01_flavor.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+
+import keystoneclient
+import keystoneclient.auth.identity.v3
+import keystoneclient.session
+import keystoneclient.v3.client
+import novaclient.client
+
+import local_settings
+
+auth = keystoneclient.auth.identity.v3.Password(auth_url=local_settings.auth_url_v3,
+                                                username=local_settings.username,
+                                                password=local_settings.password,
+                                                user_domain_name='Default',
+                                                project_domain_name='Default',
+                                                project_name=local_settings.tenant_name)
+session = keystoneclient.session.Session(auth=auth)
+nova = novaclient.client.Client('2', session=session)
+
+print json.dumps(nova.flavors.get(1).to_dict())
+
