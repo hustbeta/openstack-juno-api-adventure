92d9b6d0714dc1e5805651e582ca7c054b98aba6
Xiaokun Zhu <betahust@gmail.com>
2015-10-15 14:12:07 +0800

    keystone v3 user example

---
+++ examples/keystone/v3/05_user.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import keystoneclient
+import keystoneclient.auth.identity.v3
+import keystoneclient.exceptions
+import keystoneclient.session
+import keystoneclient.v3.client
+
+import local_settings
+
+def get_unscoped_client():
+    keystone = keystoneclient.v3.client.Client(auth_url=local_settings.auth_url_v3,
+                                               username=local_settings.username,
+                                               password=local_settings.password,
+                                               unscoped=True)
+    return keystone
+
+def list_user_projects(keystone):
+    projects = keystone.projects.list(user=keystone.user_id)
+    return projects
+
+def main():
+    """
+    1. 从username、password获取unscoped token
+    2. 使用unscoped token获取该用户的projects列表
+    3. 使用该用户的第一个可用project生成scoped token
+    4. 试试这个scoped token吧！
+    """
+    keystone = get_unscoped_client()
+    keystone.management_url = local_settings.auth_url_v3
+    projects = list_user_projects(keystone)
+    auth = keystoneclient.auth.identity.v3.Token(auth_url=local_settings.auth_url_v3,
+                                                 token=keystone.auth_token,
+                                                 project_id=projects[0].id)
+    session = keystoneclient.session.Session(auth=auth)
+    # 注意下面的auth_url是必须的，否则会报错
+    keystone2 = keystoneclient.client.Client(auth_url=local_settings.auth_url_v3,
+                                             session=session)
+    print keystone2.users.list()
+
+main()
+
